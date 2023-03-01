e39fde3633a03d9f8db83ee083534beb98e2d765
DavitMamrikishvili <davit.mamrikishvili1@gmail.com>
2023-03-01 00:33:09 -0800

    Add test to check insertion of Events

--- src/test/kotlin/com/exactpro/th2/processor/cache/collector/ArangoDBTest.kt
+++ src/test/kotlin/com/exactpro/th2/processor/cache/collector/ArangoDBTest.kt
@@ -22,9 +22,20 @@ import com.arangodb.ArangoGraph
 import com.arangodb.entity.CollectionType
 import com.arangodb.entity.EdgeDefinition
 import com.exactpro.th2.cache.common.Arango
+import com.exactpro.th2.cache.common.ArangoCredentials
+import com.exactpro.th2.cache.common.event.Event
+import com.exactpro.th2.common.grpc.ConnectionID
+import com.exactpro.th2.common.grpc.EventID
+import com.exactpro.th2.common.grpc.EventStatus
+import com.exactpro.th2.common.utils.event.EventBatcher
+import com.exactpro.th2.processor.cache.collector.event.toCacheEvent
+import com.google.protobuf.ByteString
+import com.google.protobuf.Timestamp
+import io.grpc.Grpc
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.*
+import java.time.Instant
 
 internal class ArangoDBTest {
 
@@ -35,9 +46,17 @@ internal class ArangoDBTest {
     private val existingGraph = mock(ArangoGraph::class.java)
     private val nonexistingGraph = mock(ArangoGraph::class.java)
     private val name = "test-collection"
+    private lateinit var processor: Processor
+    private val eventBatcherMock = mock(EventBatcher::class.java)
+    private val processorEventIdMock = mock(EventID::class.java)
+    private val arangoCredentialsMock = mock(ArangoCredentials::class.java)
+    private val settings = Settings(arangoCredentialsMock)
+    private val arangoDb = mock(ArangoDB::class.java)
 
     @BeforeEach
     fun init() {
+        processor = Processor(eventBatcherMock, processorEventIdMock, settings, arangoDb)
+
         `when`(existingCollection.exists()).thenReturn(true)
         `when`(nonexistingCollection.exists()).thenReturn(false)
         `when`(existingGraph.exists()).thenReturn(true)
@@ -152,5 +171,38 @@ internal class ArangoDBTest {
         arangoDb.initGraph(name, EdgeDefinition())
         verify(existingGraph, never()).drop()
         verify(arangoDatabaseMock, times(1)).createGraph(eq(name), any(), any())
+    }
+
+    @Test
+    fun callsInsertEvents() {
+        val list = mutableListOf<GrpcEvent>()
+        for (i in 1..100) {
+            val grpcEvent = GrpcEvent.newBuilder()
+                .setId(EVENT_ID)
+                .setEndTimestamp(Timestamp.newBuilder().build())
+                .setStatus(EventStatus.SUCCESS)
+                .setName(name)
+                .setType("type")
+                .setBody(ByteString.EMPTY)
+                .build()
+            list.add(grpcEvent)
+        }
+
+        list.forEach { processor.handle(INTERVAL_EVENT_ID, it) }
+        verify(arangoDb, times(1)).insertEvents(list.map { it.toCacheEvent() })
+    }
+
+    companion object {
+        private const val BOOK_NAME = "known-book"
+        private const val SCOPE_NAME = "known-scope"
+
+        private val FROM = Instant.now()
+        private val TO = FROM.plusSeconds(1_000)
+        private val PROCESSOR_EVENT_ID = EventID.newBuilder().apply {
+            bookName = BOOK_NAME
+            scope = SCOPE_NAME
+        }.build()
+        private val EVENT_ID = EventID.newBuilder().setId("id").setBookName(BOOK_NAME).setScope(SCOPE_NAME).setStartTimestamp(Timestamp.newBuilder().build()).build()
+        private val INTERVAL_EVENT_ID = PROCESSOR_EVENT_ID.toBuilder().build()
     }
 }
