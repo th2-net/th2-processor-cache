f8a17ff4187177a696f4087bf07eb59d7efb0451
davitmamrikishvili <86967699+davitmamrikishvili@users.noreply.github.com>
2023-03-07 08:31:44 +0000

    Test grpc object conversion (#5)
    * Add comparison of more fields
    
    * Add tests for ArangoDB class
    
    * Create objects in methods
    
    * Add tests when event/messages are empty
    
    * Add test to check insertion of Events
    
    * Add test to check insertion of Messages
    
    * Refactor
    
    * Add missing license
    
    * Add tests where batches get overflown

--- src/test/kotlin/com/exactpro/th2/processor/cache/collector/event/TestCacheEvent.kt
+++ src/test/kotlin/com/exactpro/th2/processor/cache/collector/event/TestCacheEvent.kt
@@ -17,78 +17,102 @@
 package com.exactpro.th2.processor.cache.collector.event
 
 import com.exactpro.th2.cache.common.event.Event
+import com.exactpro.th2.cache.common.toArangoTimestamp
 import com.exactpro.th2.common.grpc.ConnectionID
 import com.exactpro.th2.common.grpc.EventID
 import com.exactpro.th2.common.grpc.EventStatus
 import com.exactpro.th2.common.grpc.MessageID
+import com.exactpro.th2.common.util.toInstant
 import com.exactpro.th2.common.utils.event.book
 import com.exactpro.th2.common.utils.event.scope
 import com.exactpro.th2.processor.cache.collector.GrpcEvent
 import com.google.protobuf.ByteString
 import com.google.protobuf.Timestamp
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 
 class TestCacheEvent {
-    private val book = "book"
-    private val scope = "scope"
-    private val startTimestamp = Timestamp.newBuilder()
-        .setSeconds(100)
-        .setNanos(50)
-        .build()
-    private val eventId = EventID.newBuilder()
-        .setId("eventId")
-        .setBookName(book)
-        .setScope(scope)
-        .setStartTimestamp(startTimestamp)
-        .build()
-    private val parentEventId = EventID.newBuilder()
-        .setId("2")
-        .build()
-    private val endTimestamp = Timestamp.newBuilder()
-        .setSeconds(101)
-        .setNanos(50)
-        .build()
-    private val status = EventStatus.SUCCESS
-    private val name = "name"
-    private val type = "type"
-    private val body = ByteString.EMPTY
-    private val connectionId = ConnectionID.newBuilder()
-        .setSessionAlias("session-alias")
-        .setSessionGroup("session-group")
-        .build()
-    private val messageId1 = MessageID.newBuilder()
-        .setConnectionId(connectionId)
-        .build()
-    private val grpcEvent = GrpcEvent.newBuilder()
-        .setId(eventId)
-        .setParentId(parentEventId)
-        .setEndTimestamp(endTimestamp)
-        .setStatus(status)
-        .setName(name)
-        .setType(type)
-        .setBody(body)
-        .build()
 
-    private fun compare(cacheEvent: Event) {
-        assert(cacheEvent.book == grpcEvent.book)
-        assert(cacheEvent.scope == grpcEvent.scope)
-        assert(cacheEvent.id == eventId.id)
-        assert(cacheEvent.eventName == grpcEvent.name)
-        assert(cacheEvent.eventType == grpcEvent.type)
-        assert(cacheEvent.successful == grpcEvent.isSuccess())
-        assert(cacheEvent.body == grpcEvent.body.toStringUtf8())
+    private fun compare(cacheEvent: Event, grpcEvent: com.exactpro.th2.common.grpc.Event) {
+        assertEquals(cacheEvent.book, grpcEvent.book)
+        assertEquals(cacheEvent.scope, grpcEvent.scope)
+        assertEquals(cacheEvent.id, grpcEvent.id.id)
+        assertEquals(cacheEvent.eventName, grpcEvent.name)
+        assertEquals(cacheEvent.eventType, grpcEvent.type)
+        assertEquals(cacheEvent.startTimestamp, toArangoTimestamp(grpcEvent.id.startTimestamp.toInstant()))
+        assertEquals(cacheEvent.endTimestamp, toArangoTimestamp(grpcEvent.endTimestamp.toInstant()))
+        assertEquals(cacheEvent.successful, grpcEvent.isSuccess())
+        assertEquals(cacheEvent.body, grpcEvent.body.toStringUtf8())
+        assertEquals(cacheEvent.attachedMessageIds, grpcEvent.attachedMessageIdsList.map {  messageId -> messageId.toString() }.toSet())
     }
 
     @Test
     fun `formats event id correctly`() {
-        assertTrue(eventId.format() == "book:scope:100:50:eventId")
+        val book = "book"
+        val scope = "scope"
+        val startTimestamp = Timestamp.newBuilder()
+            .setSeconds(100)
+            .setNanos(50)
+            .build()
+        val eventId = EventID.newBuilder()
+            .setId("eventId")
+            .setBookName(book)
+            .setScope(scope)
+            .setStartTimestamp(startTimestamp)
+            .build()
+        assertEquals(eventId.format(), "book:scope:100:50:eventId")
     }
 
     @Test
     fun `converts grpc event to cache event`() {
+        val book = "book"
+        val scope = "scope"
+        val startTimestamp = Timestamp.newBuilder()
+            .setSeconds(100)
+            .setNanos(50)
+            .build()
+        val eventId = EventID.newBuilder()
+            .setId("eventId")
+            .setBookName(book)
+            .setScope(scope)
+            .setStartTimestamp(startTimestamp)
+            .build()
+        val parentEventId = EventID.newBuilder()
+            .setId("2")
+            .build()
+        val endTimestamp = Timestamp.newBuilder()
+            .setSeconds(101)
+            .setNanos(50)
+            .build()
+        val status = EventStatus.SUCCESS
+        val name = "name"
+        val type = "type"
+        val body = ByteString.EMPTY
+        val connectionId = ConnectionID.newBuilder()
+            .setSessionAlias("session-alias")
+            .setSessionGroup("session-group")
+            .build()
+        val messageId1 = MessageID.newBuilder()
+            .setConnectionId(connectionId)
+            .build()
+        val grpcEvent = GrpcEvent.newBuilder()
+            .setId(eventId)
+            .setParentId(parentEventId)
+            .setEndTimestamp(endTimestamp)
+            .setStatus(status)
+            .setName(name)
+            .setType(type)
+            .setBody(body)
+            .build()
         val cacheEvent = grpcEvent.toCacheEvent()
-        compare(cacheEvent)
+        compare(cacheEvent, grpcEvent)
+    }
+
+    @Test
+    fun `converts empty grpc event to cache event`() {
+        val grpcEvent = GrpcEvent.newBuilder().build()
+        val cacheEvent = grpcEvent.toCacheEvent()
+        compare(cacheEvent, grpcEvent)
     }
 }
